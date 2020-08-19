abf128ea82f3c0c36f568027dc9e5d2004cec2c7
miso.dev <miso.dev@qlcinc.co.kr>
2020-08-19 18:21:11 +0900

    Add onboarding ui

--- app/src/main/java/com/showmiso/swipecontacts/OnBoardingActivity.kt
+++ app/src/main/java/com/showmiso/swipecontacts/OnBoardingActivity.kt
@@ -2,10 +2,63 @@ package com.showmiso.swipecontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_on_boarding.*
 
 class OnBoardingActivity : AppCompatActivity() {
+    private val pagerAdapter: ViewPagerAdapter by lazy { ViewPagerAdapter(baseContext) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
+
+        view_pager.adapter = pagerAdapter
+        view_pager.addOnPageChangeListener(onPageChangeListener)
+        tab_selector.setupWithViewPager(view_pager, true)
+        btn_skip.setOnClickListener(onClickListener)
     }
+
+    private val onPageChangeListener = object : ViewPager.OnPageChangeListener {
+        override fun onPageScrollStateChanged(state: Int) {
+        }
+
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+        }
+
+        override fun onPageSelected(position: Int) {
+            when (position) {
+                (pagerAdapter.count - 1) -> {
+                    // last item
+                    btn_next.text = getString(R.string.on_boarding_start)
+                }
+            }
+        }
+    }
+
+    private val onClickListener = View.OnClickListener {
+        when (it.id) {
+            R.id.btn_skip -> {
+                skipToMain()
+            }
+            R.id.btn_next -> {
+                if (view_pager.currentItem == pagerAdapter.count - 1) {
+                    // skip
+                    skipToMain()
+                } else {
+                    // next
+
+                }
+            }
+        }
+    }
+
+    private fun skipToMain() {
+
+    }
+
 }
---
+++ app/src/main/java/com/showmiso/swipecontacts/ViewPagerAdapter.kt
@@ -0,0 +1,32 @@
+package com.showmiso.swipecontacts
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+
+class ViewPagerAdapter(
+    private val context: Context
+) : PagerAdapter() {
+    private val pageCount: Int = 4
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.layout_view_pager, container, false)
+
+        return view
+    }
+
+
+    override fun getCount(): Int {
+        return pageCount
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object` as View
+    }
+}
